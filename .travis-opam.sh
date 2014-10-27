cc52fd2e1aab10180932e0e4afc49762e0b7ef2a
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-10-27 14:32:03 +0000

    Support multiple OCaml versions

--- .travis-opam.sh
+++ .travis-opam.sh
@@ -1,5 +1,13 @@
+case "$OCAML_VERSION" in
+3.12) ppa=avsm/ocaml312+opam12 ;;
+4.00) ppa=avsm/ocaml40+opam12  ;;
+4.01) ppa=avsm/ocaml41+opam12  ;;
+4.02) ppa=avsm/ocaml42+opam12  ;;
+*)    ppa=avsm/ocaml42+opam12  ;;
+esac
+
 sudo add-apt-repository "deb mirror://mirrors.ubuntu.com/mirrors.txt trusty main restricted universe"
-sudo add-apt-repository --yes ppa:avsm/ocaml42+opam12
+sudo add-apt-repository --yes ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -y ocaml-compiler-libs ocaml-interp ocaml-base-nox ocaml-base ocaml ocaml-nox ocaml-native-compilers camlp4 camlp4-extra opam
 

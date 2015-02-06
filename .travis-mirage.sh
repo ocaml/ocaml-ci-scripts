772fd928c8e864a75695a28950936b3e9730f543
Richard Mortier <mort@cantab.net>
2015-02-06 12:27:39 +0000

    Extend `mirage` script to handle deployments

--- .travis-mirage.sh
+++ .travis-mirage.sh
@@ -2,14 +2,54 @@
 
 set -ue
 
-## first, fetch+execute the OCaml/opam setup script
+## fetch+execute the OCaml/opam setup script
 wget https://github.com/mor1/ocaml-travisci-skeleton/.travis-ocaml.sh
 sh .travis-ocaml.sh
 
-## then, install mirage
+## install mirage
 export OPAMYES=1
 eval $(opam config env)
-
 opam install mirage
+
+## copy travis `config.ml` in place
+cp .travis-config.ml src/config.ml
 make configure
-make
+make build
+
+## stash deployment build if specified
+if [ "$DEPLOY" = "1" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    # get the secure key out for deployment
+    mkdir -p ~/.ssh
+    SSH_DEPLOY_KEY=~/.ssh/id_dsa
+    travis-senv decrypt > $SSH_DEPLOY_KEY
+    chmod 600 $SSH_DEPLOY_KEY
+
+    echo "Host mir-deploy github.com" >> ~/.ssh/config
+    echo "   Hostname github.com" >> ~/.ssh/config
+    echo "   StrictHostKeyChecking no" >> ~/.ssh/config
+    echo "   CheckHostIP no" >> ~/.ssh/config
+    echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
+
+    git config --global user.email "travis@openmirage.org"
+    git config --global user.name "Travis the Build Bot"
+    git clone git@mir-deploy:${TRAVIS_REPO_SLUG}-deployment
+
+    XENIMG=mir-${TRAVIS_REPO_SLUG#mirage/mirage-}.xen
+    case "$MIRAGE_BACKEND" in
+        xen)
+            cd ${TRAVIS_REPO_SLUG}-deployment
+            rm -rf xen/$TRAVIS_COMMIT
+            mkdir -p xen/$TRAVIS_COMMIT
+            cp ../src/$XENIMG ../src/config.ml xen/$TRAVIS_COMMIT
+            bzip2 -9 xen/$TRAVIS_COMMIT/$XENIMG
+            echo $TRAVIS_COMMIT > xen/latest
+            git add xen/$TRAVIS_COMMIT xen/latest
+            ;;
+        *)
+            echo unsupported deploy mode: $MIRAGE_BACKEND
+            exit 1
+            ;;
+    esac
+    git commit -m "adding $TRAVIS_COMMIT for $MIRAGE_BACKEND"
+    git push
+fi

bd755ab9c1ca63bccf642e2c6da3f1d53b260daf
Richard Mortier <mort@cantab.net>
2015-02-15 11:38:32 +0000

    mirage: pass static ip config to `make`

--- .travis-mirage.sh
+++ .travis-mirage.sh
@@ -11,7 +11,9 @@ export OPAMYES=1
 eval $(opam config env)
 opam install mirage
 
-DEPLOY=$DEPLOY NET=$MIRAGE_NET MODE=$MIRAGE_BACKEND make configure
+DEPLOY=$DEPLOY MODE=$MIRAGE_BACKEND NET=$MIRAGE_NET \
+      ADDR=$MIRAGE_ADDR MASK=$MIRAGE_MASK GWS=$MIRAGE_GWS \
+      make configure
 make build
 
 ## stash deployment build if specified

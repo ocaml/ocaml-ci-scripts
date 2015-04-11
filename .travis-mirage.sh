87fa6bb8daf03b41befe640f9b4f001798a080bb
Thomas Gazagnaire <thomas@gazagnaire.org>
2015-04-11 23:00:43 +0200

    shellcheck .travis-mirage.sh

--- .travis-mirage.sh
+++ .travis-mirage.sh
@@ -1,4 +1,6 @@
-## mirage setup
+#!/usr/bin/env bash
+
+### mirage setup
 
 set -ex
 
@@ -6,12 +8,12 @@ set -ex
 fork_user=${FORK_USER:-ocaml}
 
 ## fetch+execute the OCaml/opam setup script
-wget https://raw.githubusercontent.com/${fork_user}/ocaml-travisci-skeleton/master/.travis-ocaml.sh
+wget "https://raw.githubusercontent.com/${fork_user}/ocaml-travisci-skeleton/master/.travis-ocaml.sh"
 sh .travis-ocaml.sh
 
 ## install mirage
 export OPAMYES=1
-eval $(opam config env)
+eval "$(opam config env)"
 opam install mirage
 
 DEPLOY=$DEPLOY MODE=$MIRAGE_BACKEND NET=$MIRAGE_NET \
@@ -27,33 +29,33 @@ if [ "$DEPLOY" = "1" \
     # get the secure key out for deployment
     mkdir -p ~/.ssh
     SSH_DEPLOY_KEY=~/.ssh/id_dsa
-    travis-senv decrypt > $SSH_DEPLOY_KEY
-    chmod 600 $SSH_DEPLOY_KEY
+    travis-senv decrypt > "${SSH_DEPLOY_KEY}"
+    chmod 600 "${SSH_DEPLOY_KEY}"
 
-    echo "Host mir-deploy github.com" >> ~/.ssh/config
-    echo "   Hostname github.com" >> ~/.ssh/config
-    echo "   StrictHostKeyChecking no" >> ~/.ssh/config
-    echo "   CheckHostIP no" >> ~/.ssh/config
-    echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
+    { echo "Host mir-deploy github.com";
+      echo "   Hostname github.com";
+      echo "   StrictHostKeyChecking no";
+      echo "   CheckHostIP no";
+      echo "   UserKnownHostsFile=/dev/null"; } >> ~/.ssh/config
 
     git config --global user.email "travis@openmirage.org"
     git config --global user.name "Travis the Build Bot"
-    git clone git@mir-deploy:${TRAVIS_REPO_SLUG}-deployment
+    git clone "git@mir-deploy:${TRAVIS_REPO_SLUG}-deployment"
 
     DEPLOYD=${TRAVIS_REPO_SLUG#mirage/}-deployment
     XENIMG=mir-${TRAVIS_REPO_SLUG#mirage/mirage-}.xen
     case "$MIRAGE_BACKEND" in
         xen)
-            cd $DEPLOYD
-            rm -rf xen/$TRAVIS_COMMIT
-            mkdir -p xen/$TRAVIS_COMMIT
-            cp ../src/$XENIMG ../src/config.ml xen/$TRAVIS_COMMIT
-            bzip2 -9 xen/$TRAVIS_COMMIT/$XENIMG
-            echo $TRAVIS_COMMIT > xen/latest
-            git add xen/$TRAVIS_COMMIT xen/latest
+            cd "${DEPLOYD}"
+            rm -rf "xen/${TRAVIS_COMMIT}"
+            mkdir -p "xen/${TRAVIS_COMMIT}"
+            cp "../src/${XENIMG}" ../src/config.ml "xen/${TRAVIS_COMMIT}"
+            bzip2 -9 "xen/${TRAVIS_COMMIT}/${XENIMG}"
+            echo "${TRAVIS_COMMIT}" > xen/latest
+            git add "xen/${TRAVIS_COMMIT}" xen/latest
             ;;
         *)
-            echo unsupported deploy mode: $MIRAGE_BACKEND
+            echo unsupported deploy mode: "${MIRAGE_BACKEND}"
             exit 1
             ;;
     esac

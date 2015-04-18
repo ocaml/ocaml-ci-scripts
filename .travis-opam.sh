02f5ca4ff0dc783f8b864b36ef8d7fe3ff95946d
David Sheets <sheets@alum.mit.edu>
2015-04-18 15:57:01 +0100

    Add FORK_BRANCH

--- .travis-opam.sh
+++ .travis-opam.sh
@@ -1,5 +1,8 @@
-# If a fork of these scripts are specified, use that GitHub user instead
+# If a fork of these scripts is specified, use that GitHub user instead
 fork_user=${FORK_USER:-ocaml}
+
+# If a branch of these scripts is specified, use that branch instead of 'master'
+fork_branch=${FORK_BRANCH:-master}
 
 ### Bootstrap
 
@@ -7,7 +10,7 @@ set -uex
 unset TESTS
 
 get() {
-  wget https://raw.githubusercontent.com/${fork_user}/ocaml-travisci-skeleton/master/$@
+  wget https://raw.githubusercontent.com/${fork_user}/ocaml-travisci-skeleton/${fork_branch}/$@
 }
 
 TMP_BUILD=$(mktemp -d)

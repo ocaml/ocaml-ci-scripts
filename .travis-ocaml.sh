cdc72bfeca6e9a62b4e494b234f27574b3ffbe38
Thomas Leonard <talex5@gmail.com>
2015-05-13 12:07:22 +0100

    Get opam through 0install
    Ubuntu version lacks aspcud and gives poor solutions.

--- .travis-ocaml.sh
+++ .travis-ocaml.sh
@@ -2,6 +2,14 @@
 
 set -uex
 
+wget https://downloads.sourceforge.net/project/zero-install/0install/2.8/0install-linux-x86_64-2.8.tar.bz2
+tar xjf 0install-linux-x86_64-2.8.tar.bz2
+cd 0install-linux-x86_64-2.8
+./install.sh home
+export PATH=$HOME/bin:$PATH
+0install add opam http://tools.ocaml.org/opam.xml
+
+
 # the base opam repository to use for bootstrapping and catch-all namespace
 BASE_REMOTE=${BASE_REMOTE:-git://github.com/ocaml/opam-repository}
 

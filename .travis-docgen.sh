5c026ad3a12c819d1b32200a76b2f503e2be836a
Si Beaumont <simon.beaumont@citrix.com>
2015-06-15 14:41:57 +0100

    docgen: git-add docs if they're not already there
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- .travis-docgen.sh
+++ .travis-docgen.sh
@@ -27,5 +27,6 @@ cp _build/pci.docdir/* $DOCDIR
 
 git -C $DOCDIR config user.email "travis@travis-ci.org"
 git -C $DOCDIR config user.name "Travis"
-git -C $DOCDIR commit --allow-empty -am "Travis build $TRAVIS_BUILD_NUMBER pushed docs to gh-pages"
+git -C $DOCDIR add .
+git -C $DOCDIR commit --allow-empty -m "Travis build $TRAVIS_BUILD_NUMBER pushed docs to gh-pages"
 git -C $DOCDIR push origin gh-pages > /dev/null

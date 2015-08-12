006e5efefa61ec4d0cb1e53677e1edada1b24177
Thomas Gazagnaire <thomas@gazagnaire.org>
2015-08-12 08:46:31 +0100

    Explicitely install OCaml 4.02.3 when using older ppas (where only 4.02.1 is available)

--- .travis-ocaml.sh
+++ .travis-ocaml.sh
@@ -35,9 +35,9 @@ install_on_linux () {
     3.12,1.2.2) ppa=avsm/ocaml312+opam12 ;;
     4.00,1.2.2) ppa=avsm/ocaml40+opam12 ;;
     4.01,1.2.2) ppa=avsm/ocaml41+opam12 ;;
-    4.02,1.1.2) ppa=avsm/ocaml42+opam11 ;;
-    4.02,1.2.0) ppa=avsm/ocaml42+opam120 ;;
-    4.02,1.2.1) ppa=avsm/ocaml42+opam121 ;;
+    4.02,1.1.2) OPAM_SWITCH=4.02.3; ppa=avsm/ocaml42+opam11 ;;
+    4.02,1.2.0) OPAM_SWITCH=4.02.3; ppa=avsm/ocaml42+opam120 ;;
+    4.02,1.2.1) OPAM_SWITCH=4.02.3; ppa=avsm/ocaml42+opam121 ;;
     4.02,1.2.2) ppa=avsm/ocaml42+opam12 ;;
     4.03,1.2.2) OCAML_VERSION=4.02; OPAM_SWITCH="4.03.0dev+trunk"; ppa=avsm/ocaml42+opam12 ;;
     *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;

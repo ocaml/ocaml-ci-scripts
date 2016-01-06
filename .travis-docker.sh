00539a18822920effa3d7d902508fedcc9e221ce
Anil Madhavapeddy <anil@recoil.org>
2016-01-06 16:48:36 +0000

    Add a Docker-based Travis tester that invokes `travis-opam`

---
+++ .travis-docker.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -e
+# To use this, run `opam travis --help`
+
+echo -en 'travis_fold:start:script.1\\r'
+# create env file
+echo PACKAGE="$PACKAGE" > env.list
+echo EXTRA_REMOTES="$EXTRA_REMOTES" >> env.list
+echo PINS="$PINS" >> env.list
+echo INSTALL="$INSTALL" >> env.list
+echo DEPOPTS="$DEPOPTS" >> env.list
+echo TESTS="$TESTS" >> env.list
+echo REVDEPS="$REVDEPS" >> env.list
+echo EXTRA_DEPS="$EXTRA_DEPS" >> env.list
+echo PRE_INSTALL_HOOK="$PRE_INSTALL_HOOK" >> env.list
+echo POST_INSTALL_HOOK="$POST_INSTALL_HOOK" >> env.list
+
+# build a local image to trigger any ONBUILDs
+echo FROM ocaml/opam:${DISTRO}_ocaml-${OCAML_VERSION} > Dockerfile
+echo VOLUME /repo >> Dockerfile
+echo WORKDIR /repo >> Dockerfile
+docker build -t local-build .
+echo -en 'travis_fold:end:script.1\\r'
+
+# run travis-opam with the local repo volume mounted
+OS=~/build/$TRAVIS_REPO_SLUG
+chmod -R a+w $OS
+docker run --env-file=env.list -v ${OS}:/repo local-build travis-opam
+

81e388e7a12faeb3f85c5932a68f49076f364e87
Török Edwin <edwin@etorok.net>
2017-01-09 22:40:45 +0200

    use depext when installing travis-opam
    needed if m4 is missing, and with the upcoming jq change.

--- .travis-docker.sh
+++ .travis-docker.sh
@@ -25,9 +25,11 @@ echo WORKDIR /home/opam/opam-repository >> Dockerfile
 echo RUN git pull origin master >> Dockerfile
 
 if [ $fork_user != $default_user -o $fork_branch != $default_branch ]; then
-    echo RUN opam pin add travis-opam \
+    echo RUN opam remove travis-opam >> Dockerfile
+    echo RUN opam pin add -n travis-opam \
          https://github.com/$fork_user/ocaml-ci-scripts.git#$fork_branch \
          >> Dockerfile
+    echo RUN opam depext -i travis-opam >> Dockerfile
 fi
 
 echo RUN opam update -u -y >> Dockerfile

5d572e66d881e1113741a04dfe653c9e4793a009
Kate <kit.ty.kate@disroot.org>
2020-03-18 20:48:46 +0000

    Allow custom versions of opam to be requested (but forbid versions 1.x)

--- .travis-ocaml.sh
+++ .travis-ocaml.sh
@@ -26,7 +26,7 @@ OPAM_LATEST_RELEASE=2.0.6
 
 case $OPAM_VERSION in
     2|2.0) OPAM_VERSION=$OPAM_LATEST_RELEASE;;
-    *) echo "Opam version '$OPAM_VERSION' is not supported"; exit 1;;
+    1.*) echo "Opam version '$OPAM_VERSION' is not supported"; exit 1;;
 esac
 
 if [ "$TRAVIS_OS_NAME" = "osx" ] ; then
